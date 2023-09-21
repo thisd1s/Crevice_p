12ea6654461d41344201ade4061f3ce37b79bc3c
thisd1s <distribute0940@naver.com>
2023-09-21 21:52:43 +0900

    asdf

---
+++ script.js
@@ -0,0 +1,17 @@
+alert("322ho DDAL JAP E 2 eun ho");
+const hello = document.getElementById('hi');
+hello.style.color = "red";
+button = document.getElementById('button');
+button.addEventListener("click", () => {
+    alert("은호 1일 3명의 딸 낳음");
+});
+
+function enuho() {
+    alert("은호 오빠 찌찌 파티");
+}
+setInterval(enuho, 100000000);
+
+clearInterval(enuho); //종료
+
+//hello.innerText = 'fkjflkasjflaksfjaslk';
+//alert(input.value);
